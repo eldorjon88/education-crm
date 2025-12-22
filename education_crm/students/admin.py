3b54f2c87d5b1423c1e516f7a072db81d38f1939
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-12-22 14:24:57 +0500

    crm

--- education_crm/students/admin.py
+++ education_crm/students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'phone', 'status', 'is_risk', 'created_at')
+    list_filter = ('status', 'is_risk')
+    search_fields = ('full_name', 'phone')
--- education_crm/students/models.py
+++ education_crm/students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+    )
+
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='active'
+    )
+    is_risk = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
